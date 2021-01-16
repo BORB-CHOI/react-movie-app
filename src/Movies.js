90fd4c5406db99428d1a77be4d9feab21ce1f595
BORB <qzsec299@naver.com>
2021-01-16 22:06:17 +0900

    Responsive css

--- src/Movies.js
+++ src/Movies.js
@@ -1,22 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movies.css";
 
 const Movies = ({ year, title, summary, poster, genres }) => {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title}></img>
-      <div className="movie_data">
+      <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres">
           {genres.map((genre, index) => (
             // map 함수는 item 말고도 item number도 반환해 줌
             <li key={index} className="genres__genre">
-              {genre}
+              {console.log(genres[genres.length - 1])}
+              {console.log(genre)}
+              {genres[genres.length - 1] === genre ? genre : `${genre},`}
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <span className="movie__summary">
+          {summary.slice(0, 300)} <span className="summar_more"> · · ·</span>
+        </span>
       </div>
     </div>
   );
